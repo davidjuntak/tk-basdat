0d8e921c371b007b5e357988f7606ac1712fe8fc
David Michael Simanjuntak <david.juntak11@gmail.com>
2018-05-21 23:15:47 +0700

    Update profil

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -76,7 +76,7 @@
             <!-- Content Header (Page header) -->
             <section class="content-header">
                 <h1>
-                    Profil Relawan
+                    Profil Pengguna
                 </h1>
             </section>
 
